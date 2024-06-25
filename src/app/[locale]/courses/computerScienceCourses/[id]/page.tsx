fe0045cfa875f81e13dd0c4a8c6fc92a4c775174
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-06-25 23:39:27 +0200

    feat: detail courses

---
+++ src/app/[locale]/courses/computerScienceCourses/[id]/page.tsx
@@ -0,0 +1,85 @@
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export function generateStaticParams() {
+  return [
+    { locale: "de", id: "0" },
+    { locale: "de", id: "1" },
+
+    { locale: "en", id: "0" },
+    { locale: "en", id: "1" },
+  ];
+}
+
+export default async function detailPage({
+  params: { locale, id },
+}: {
+  params: { locale: string; id: string };
+}) {
+  unstable_setRequestLocale(locale);
+  const translation = (
+    await import(`../../../../../../locales/${locale}/${locale}.json`)
+  ).default;
+  const course = translation.ComputerScienceCourses.courses[id];
+  return (
+    <main className="">
+      <div className="bg-darkblue pb-16">
+        <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4 ">
+          <div className="rounded-full bg-yellow p-2 w-36 text-p-sm text-darkblue font-bold font-palanquin mb-8">
+            <p className="text-center">{course.badge}</p>
+          </div>
+          <h3 className="lg:text-h-xl  text-h-l  text-yellow font-palanquin md:text-left text-center mb-4 whitespace-pre-line">
+            {course.title}
+          </h3>
+          <p className="text-white max-w-5xl font-palanquin text-p-lg md:text-h-xs">
+            {course.description}
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4 font-palanquin">
+        <div className="">
+          <h4 className="lg:text-h-l  text-h-md  text-darkblue  md:text-left text-center mb-4 whitespace-pre-line">
+            {course.whatYouLearnTitle}
+          </h4>
+          <p className="text-p-lg md:text-h-xs text-darkblue mb-6">
+            {course.whatYouLearnText}
+          </p>
+          <ul className=" list-disc ml-8 text-p-lg md:text-h-xs text-darkblue ">
+            {course.whatYouLearnList.map((item: string, index: number) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-12">
+          <h4 className="lg:text-h-l  text-h-md  text-darkblue  md:text-left text-center mb-4 whitespace-pre-line">
+            {course.workpiecesTitle}
+          </h4>
+
+          <ul className=" list-disc ml-8 text-p-lg md:text-h-xs text-darkblue ">
+            {course.workpiecesList.map((item: string, index: number) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="mt-12">
+          <h4 className="lg:text-h-l  text-h-md  text-darkblue  md:text-left text-center mb-4 whitespace-pre-line">
+            {course.courseOrganizationTitle}
+          </h4>
+          <p className="text-p-lg md:text-h-xs text-darkblue">
+            {course.courseOrganizationText}
+          </p>
+        </div>
+        <div className="mt-12">
+          <h4 className="lg:text-h-l  text-h-md  text-darkblue  md:text-left text-center mb-4 whitespace-pre-line">
+            {course.requirementsTitle}
+          </h4>
+
+          <ul className=" list-disc ml-8 text-p-lg md:text-h-xs text-darkblue ">
+            {course.requirementsList.map((item: string, index: number) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/[locale]/courses/multimedia/page.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import titleImage from "@/assets/home/titleImage.png";
+import CoursesOverviewMultimedia from "@/components/courses/coursesOverview/coursesOverviewMultimedia";
+import MoreCoursesCarousel from "@/components/general/moreCoursesCarousel/moreCoursesCarousel";
+
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+interface detailCoursePageProps {
+  params: {
+    locale: string;
+  };
+}
+
+export default function MultimediaCourses({
+  params: { locale },
+}: detailCoursePageProps) {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("MultimediaCourses");
+
+  return (
+    <main>
+      <div className="min-h-[calc(100vh-80px)] bg-darkblue w-full flex items-center pb-8 md:-pd-0">
+        <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4 ">
+          <h1 className="lg:text-h-xl  text-h-l  text-yellow font-palanquin md:text-left text-center">
+            {t("mainTitle")}
+          </h1>
+
+          <div className="flex flex-col lg:flex-row">
+            <div className="lg:w-2/5 w-full">
+              <h4 className="text-white text-h-sm md:text-h-md mt-4 mb-4  md:text-left text-center font-palanquin !font-thin">
+                {t("subTitle")}
+              </h4>
+            </div>
+            <div className="lg:w-3/5 w-full flex justify-center">
+              <Image
+                src={titleImage}
+                alt="Picture of the author"
+                className="h-full object-contain "
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto md:py-24 py-12 px-8 lg:px-4">
+        <CoursesOverviewMultimedia lng={locale} />
+      </div>
+      {/* <div className="mb-24">
+        <MoreCoursesCarousel />
+      </div> */}
+    </main>
+  );
+}
