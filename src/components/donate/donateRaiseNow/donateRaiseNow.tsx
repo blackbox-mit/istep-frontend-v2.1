3d8c322316c2d6f0273e95bca5f09ad8220560da
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-10-23 23:09:35 +0200

    feat: raisenow new

--- src/components/donate/donateRaiseNow/donateRaiseNow.tsx
+++ src/components/donate/donateRaiseNow/donateRaiseNow.tsx
@@ -9,27 +9,9 @@ interface donateRaiseNowProps {}
 
 export default function DonateRaiseNow({}: donateRaiseNowProps) {
   const [open, setOpen] = useState(false);
-  const buttonRef = useRef(null);
-
   return (
     <>
       <div className="rounded-3xl bg-white w-full shadow-2xl font-palanquin p-8 cursor-pointer">
-        <Script
-          src="https://tamaro.raisenow.com/istep-b7da/latest/widget.js"
-          onLoad={
-            () => {
-              //@ts-ignore
-              window.rnw.tamaro.runWidget(buttonRef.current, {
-                language: "en",
-              });
-              // console.log(window.rnw.tamaro.runWidget(buttonRef));
-            }
-
-            /* window.rnw.tamaro.runWidget(".rnw-widget-container", {
-              language: "en", // You might need to replace this with the language from your store
-            })*/
-          }
-        />
         <div
           className={`flex justify-between items-center h-12 ${
             open ? "mb-6" : ""
@@ -40,11 +22,14 @@ export default function DonateRaiseNow({}: donateRaiseNowProps) {
 
           <ChevronDownIcon aria-hidden="true" className="w-12" />
         </div>
-
-        <div
-          ref={buttonRef}
-          className={`rnw-widget-container ", ${open ? "visible" : "hidden"}`}
-        />
+        {open && (
+          <iframe
+            src="https://donate.raisenow.io/zbvxd"
+            width="100%"
+            height="1400px"
+            frameBorder="0"
+          ></iframe>
+        )}
       </div>
     </>
   );
