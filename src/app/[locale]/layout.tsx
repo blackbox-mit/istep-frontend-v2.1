64dd5219265d7bc1f1ae6bb92b80748ab4330d89
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-12-03 09:41:40 +0100

    feat: donate button englisch

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -30,10 +30,11 @@ export default async function RootLayout({
   params: { locale },
 }: Props) {
   unstable_setRequestLocale(locale);
+  const messages = require(`../../../locales/${locale}/${locale}.json`);
   return (
     <html lang={locale}>
       <body className="h-full">
-        <NextIntlClientProvider locale={locale}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <Suspense>
             <Navbar />
           </Suspense>
