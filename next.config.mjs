2618bdb1745d442e02aa0ea42807a7f0cde4c568
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2025-02-07 13:16:12 +0100

    feat: unopt images

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
